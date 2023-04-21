b92cf6c0244fe270f9057ae0cf52de36b01878c1
Pinpunyu <pe25617259@gmail.com>
2023-04-21 23:03:24 +0800

    add decrypt

--- Transposition_Cipher3.py
+++ Transposition_Cipher3.py
@@ -1,6 +1,7 @@
 import functools
 
 class Transposition_Cipher3D:
+
     three = ((0,0), (0,2), (0,4), (1,0), (1,1), (1,2), (1,3), (1,4))
     four  = ((0,0), (0,1), (0,2), (1,2), (2,0), (2,1), (2,2), (2,3))
     five  = ((0,0), (0,1), (0,2), (0,4), (1,0), (1,2), (1,3), (1,4))
@@ -14,71 +15,132 @@ class Transposition_Cipher3D:
         #明文儲存的3維
         dim3D = []
 
-        for idx, ascii in enumerate(key):
+        for zidx, ascii in enumerate(key):
 
             # key轉成ASCii二進位
-            print(f"key[{idx}] = {ascii}")
+            # print(f"key[{idx}] = {ascii}")
             ascii = ord(ascii)
             key_bin = format(int(ascii), "b").zfill(8)
-            print(f"{ascii} = {key_bin}")
+            # print(f"{ascii} = {key_bin}")
 
-            # plain轉成ASCii二進位
-            num = ord(plaintext[idx])
-            num = format(int(num), "b").zfill(8)
-            print(f"plain[{idx}] = {num}")
+            # plaintext轉成ASCii二進位
+            plain_ascii = ord(plaintext[zidx])
+            plain_ascii = format(int(plain_ascii), "b").zfill(8)
+            # print(f"plain[{idx}] = {num}")
 
             # key十位數代表形狀，個位數代表明文開始idx
             step = int(key_bin[4:8], 2)
-            pos = int(key_bin[0:4], 2)
-            print(str(pos)+' '+str(step))
+            number = int(key_bin[0:4], 2)
+            # print(str(pos)+' '+str(step))
 
-            for idx2, ii in enumerate(Transposition_Cipher3D.key_shape[pos]):       
-                dim3D.append([ii[0], ii[1], idx , num[(idx2+step)%8]])
+            for plain_idx, pos in enumerate(Transposition_Cipher3D.key_shape[number]):       
+                dim3D.append([pos[0], pos[1], zidx , plain_ascii[(plain_idx+step)%8]])
                 
+        
 
-        dim3D = sorted(dim3D, key=functools.cmp_to_key(Transposition_Cipher3D.encrypt_cmp))
+        sort_dim3D = sorted(dim3D, key=functools.cmp_to_key(Transposition_Cipher3D.__encrypt_cmp__))
+
+        # for i in range(0,len(dim3D)):
+        #     if i%8 == 0: print("-----------")
+        #     print(dim3D[i], sort_dim3D[i])
 
         cipher_str = ""
-        for bit in dim3D:
+        for bit in sort_dim3D:
             cipher_str += bit[3]
 
         cipher = ""
         for idx in range(0 , len(cipher_str) , 8):
             # Getting the ASCII value
             cipher += chr(int(cipher_str[idx:idx+8],2))
-            print("{:02x}".format(int(cipher_str[idx:idx+8],2)) , int(cipher_str[idx:idx+8],2))
+            # print("{:02x}".format(int(cipher_str[idx:idx+8],2)) , int(cipher_str[idx:idx+8],2))
 
                 
         return cipher
         
-    def decrypt(plaintext:str , key:str) -> str:
-        pass
-    def encrypt_cmp(a:list, b:list) -> bool:
-        if a[0] != b[0]:
-            rel = 1 if a[0] > b[0] else -1
-            return rel
+    def decrypt(ciphertext:str , key:str) -> str:
+        
+        dim3D = [] # key先建構3維
+        total_step = [] # 根據不同key密文要移動的步數
+        total_cipher = "" # 密文排成一排
+
+        for zidx, ascii in enumerate(key):
+
+            # key轉成ASCii二進位
+            # print(f"key[{idx}] = {ascii}")
+            ascii = ord(ascii)
+            key_bin = format(int(ascii), "b").zfill(8)
+            # print(f"{ascii} = {key_bin}")
+
+            # ciphertext轉成ASCii二進位
+            cipher_ascii = ord(ciphertext[zidx])
+            cipher_ascii = format(int(cipher_ascii), "b").zfill(8)
+            total_cipher += cipher_ascii
+            # print(f"plain[{zidx}] = {cipher_ascii}")
+
+            # key十位數代表形狀，個位數代表明文開始idx
+            step = int(key_bin[4:8], 2)
+            total_step.append(step)
+            number = int(key_bin[0:4], 2)
+            # print(str(pos)+' '+str(step))
+
+            # 畫出key的3D陣列
+            for pos in Transposition_Cipher3D.key_shape[number]:       
+                dim3D.append([pos[0], pos[1], zidx , ""])
+
+         
+        dim3D = sorted(dim3D, key=functools.cmp_to_key(Transposition_Cipher3D.__encrypt_cmp__))
+
+
+        for idx, cipher_info in enumerate(dim3D):
+            cipher_info[3] = total_cipher[idx]
+
+        sort_dim3D = sorted(dim3D, key=functools.cmp_to_key(Transposition_Cipher3D.__decrypt_cmp__))
+
+
+        cipher_to_plain = ""
+        cipher_to_plain = cipher_to_plain.zfill(len(dim3D))
+
+
+        plain_str = ""
+        for idx in range(0, len(sort_dim3D)):        
+            number = int(idx/8)*8 # 第幾個明文
+            plain_str += sort_dim3D[number + (8-total_step[int(idx/8)]%8+idx)%8][3]
+
+        # print(plain_str)
+        
+        plain = ""
+        for idx in range(0 , len(plain_str) , 8):
+            # Getting the ASCII value
+            plain += chr(int(plain_str[idx:idx+8],2))
+            # print("{:02x}".format(int(plain_str[idx:idx+8],2)) , int(plain_str[idx:idx+8],2))
+
+                
+        return plain
+
+
+    def __encrypt_cmp__(a:list, b:list) -> bool:
         if a[1] != b[1]:
             rel = 1 if a[1] > b[1] else -1
             return rel
-        if a[2] != b[2]:
-            rel = 1 if a[2] < b[2] else -1
-            return rel
-    
-    def decrypt_cmp(a:list, b:list) -> bool:
-        if a[2] != b[2]:
-            rel = 1 if a[2] < b[2] else -1
-            return rel
-        if a[1] != b[1]:
-            rel = 1 if a[1] > b[1] else -1
-            return rel
         if a[0] != b[0]:
             rel = 1 if a[0] > b[0] else -1
+            return rel
+        if a[2] != b[2]:
+            rel = 1 if a[2] < b[2] else -1
             return rel
         
+    def __decrypt_cmp__(a:list, b:list) -> bool:
+        if a[2] != b[2]:
+            rel = 1 if a[2] > b[2] else -1
+            return rel
+        if a[0] != b[0]:
+            rel = 1 if a[0] > b[0] else -1
+            return rel
+        if a[1] != b[1]:
+            rel = 1 if a[1] > b[1] else -1
+            return rel
 
-    
-# arr1 = np.array([[[2,17], [45, 78]], [[88, 92], [60, 76]],[[76,33],[20,18]]])
-# print("Create 3-d array:",arr1)
+   
 
 # three = ('11','01','11','01','11')
 # four  = ('0010','0110','1111','0010')
@@ -88,67 +150,11 @@ class Transposition_Cipher3D:
 
 # key_num = (three, four, five, six, seven)
 
-#加密形狀
-# three = ((0,0), (0,2), (0,4), (1,0), (1,1), (1,2), (1,3), (1,4))
-# four  = ((0,0), (0,1), (0,2), (1,2), (2,0), (2,1), (2,2), (2,3))
-# five  = ((0,0), (0,1), (0,2), (0,4), (1,0), (1,2), (1,3), (1,4))
-# six   = ((0,0), (0,1), (0,2), (0,3), (0,4), (1,0), (1,3), (1,4))
-# seven = ((0,0), (0,1), (0,2), (1,0), (2,0), (2,1), (2,2), (2,3))
 
-# key_shape = {3:three, 4:four, 5:five, 6:six, 7:seven}
+plaintext = "a1095514"
+key       = "aaaaaaaa"
+print(f"plaintext = {plaintext}, key = {key}")
 
-# #明文 金鑰
-# plaintext = "11"
-# key =  "11"
-
-# #明文儲存的3維
-# dim3D = []
-
-# for idx, ascii in enumerate(key):
-
-#     # key轉成ASCii二進位
-#     print(f"key[{idx}] = {ascii}")
-#     ascii = ord(ascii)
-#     key_bin = format(int(ascii), "b").zfill(8)
-#     print(f"{ascii} = {key_bin}")
-
-#     # plain轉成ASCii二進位
-#     num = ord(plaintext[idx])
-#     num = format(int(num), "b").zfill(8)
-#     print(f"plain[{idx}] = {num}")
-
-#     # key十位數代表形狀，個位數代表明文開始idx
-#     step = int(key_bin[4:8], 2)
-#     pos = int(key_bin[0:4], 2)
-#     print(str(pos)+' '+str(step))
-
-#     for idx2, ii in enumerate(key_shape[pos]):       
-#         dim3D.append([ii[0], ii[1], idx , num[(idx2+step)%8]])
-        
-
-# dim3D = sorted(dim3D, key=functools.cmp_to_key(encrypt_cmp))
-
-
-
-# cipher_str = ""
-# for bit in dim3D:
-#     cipher_str += bit[3]
-
-# cipher = ""
-# for idx in range(0 , len(cipher_str) , 8):
-#     # Getting the ASCII value
-#     cipher += chr(int(cipher_str[idx:idx+8],2))
-#     print("{:02x}".format(int(cipher_str[idx:idx+8],2)) , int(cipher_str[idx:idx+8],2))
-
-plaintext = "11"
-key =  "11"
-
-cipher = Transposition_Cipher3D.encrypt(plaintext, key)
-# print(cipher)
-
-
- 
-
-
-
-
+ciphertext_input = Transposition_Cipher3D.encrypt(plaintext, key)
+plaintext_output = Transposition_Cipher3D.decrypt(ciphertext_input, "aaaa0aa0")
+print(f"plaintext = {plaintext_output}")
