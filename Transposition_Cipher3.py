5d97d3ace2b2fbffb1040aa620f7b1f57fce6941
Pinpunyu <pe25617259@gmail.com>
2023-04-29 14:13:26 +0800

    Transmission by ascii && Fefense frequency analysis

--- Transposition_Cipher3.py
+++ Transposition_Cipher3.py
@@ -12,23 +12,23 @@ class Transposition_Cipher3D:
     
     key_shape = {3:three, 4:four, 5:five, 6:six, 7:seven}
 
+
     def encrypt(plaintext:str , key:str) -> str:
+        
+        dim3D = [] # 明文儲存的3維
+        key_size = len(key) # key的長度
 
-        #明文儲存的3維
-        dim3D = []
-
-        for zidx, ascii in enumerate(key):
-
-            # key轉成ASCii二進位
-            # print(f"key[{idx}] = {ascii}")
-            ascii = ord(ascii)
-            key_bin = format(int(ascii), "b").zfill(8)
-            # print(f"{ascii} = {key_bin}")
+        for zidx, plain_graphics in enumerate(plaintext):
 
             # plaintext轉成ASCii二進位
-            plain_ascii = ord(plaintext[zidx])
+            plain_ascii = ord(plain_graphics)
             plain_ascii = format(int(plain_ascii), "b").zfill(8)
-            # print(f"plain[{idx}] = {num}")
+
+            # key轉成ASCii二進位，key會再加上index，避免頻率破解法
+            key_ascii = ord(key[zidx % key_size])
+            key_ascii = ((key_ascii + zidx) % 127) # 控制在ASCII可顯示字元範圍內
+            if key_ascii < 48 : key_ascii += 48
+            key_bin = format(int(key_ascii), "b").zfill(8)
 
             # key十位數代表形狀，個位數代表明文開始idx
             step = int(key_bin[4:8], 2)
@@ -38,7 +38,6 @@ class Transposition_Cipher3D:
             for plain_idx, pos in enumerate(Transposition_Cipher3D.key_shape[number]):       
                 dim3D.append([pos[0], pos[1], zidx , plain_ascii[(plain_idx+step)%8]])
                 
-        
 
         sort_dim3D = sorted(dim3D, key=functools.cmp_to_key(Transposition_Cipher3D.__encrypt_cmp__))
 
@@ -51,45 +50,41 @@ class Transposition_Cipher3D:
             cipher_str += bit[3]
 
         cipher = ""
-        cipher_hex = ""
         for idx in range(0 , len(cipher_str) , 8):
             # Getting the ASCII value
             cipher += chr(int(cipher_str[idx:idx+8],2))
-            cipher_hex += "{:02x}".format(int(cipher_str[idx:idx+8],2))
-            # print(type(int(cipher_str[idx:idx+8],2)))
             # print("{:02x}".format(int(cipher_str[idx:idx+8],2)) , int(cipher_str[idx:idx+8],2))
 
                 
-        return cipher_hex
+        return cipher
         
     def decrypt(ciphertext:str , key:str) -> str:
         
         dim3D = [] # key先建構3維
+        key_size = len(key) # key的長度
         total_step = [] # 根據不同key密文要移動的步數
         total_cipher = "" # 密文排成一排
 
-        for zidx, ascii in enumerate(key):
+        for zidx in range(0, len(ciphertext)):
 
             # key轉成ASCii二進位
             # print(f"key[{idx}] = {ascii}")
-            ascii = ord(ascii)
-            key_bin = format(int(ascii), "b").zfill(8)
+            key_ascii = ord(key[zidx % key_size])
+            key_ascii = ((key_ascii + zidx) % 127) # 控制在ASCII可顯示字元範圍內
+            if key_ascii < 48 : key_ascii += 48
+            key_bin = format(int(key_ascii), "b").zfill(8)
             # print(f"{ascii} = {key_bin}")
-
-            # ciphertext(16進位)轉成ASCii二進位
-            # print(ciphertext[2*zidx:2*zidx+2])
-            
-
-            byte_string = binascii.unhexlify(ciphertext[2*zidx:2*zidx+2])
-            cipher_ascii = format(bin(int.from_bytes(byte_string, byteorder='big')).lstrip('0b')).zfill(8)
-            total_cipher += cipher_ascii
-            # print(f"plain[{zidx}] = {cipher_ascii}")
 
             # key十位數代表形狀，個位數代表明文開始idx
             step = int(key_bin[4:8], 2)
             total_step.append(step)
             number = int(key_bin[0:4], 2)
             # print(str(pos)+' '+str(step))
+
+            # ciphertext轉成ASCii二進位
+            cipher_ascii = ord(ciphertext[zidx])
+            cipher_ascii = format(int(cipher_ascii), "b").zfill(8)
+            total_cipher += cipher_ascii
 
             # 畫出key的3D陣列
             for pos in Transposition_Cipher3D.key_shape[number]:       
@@ -115,17 +110,14 @@ class Transposition_Cipher3D:
             plain_str += sort_dim3D[number + (8-total_step[int(idx/8)]%8+idx)%8][3]
 
         # print(plain_str)
-        
+
         plain = ""
-        plain_hex = ""
         for idx in range(0 , len(plain_str) , 8):
             # Getting the ASCII value
             plain += chr(int(plain_str[idx:idx+8],2))
-            plain_hex += "{:02x}".format(int(plain_str[idx:idx+8],2))
             # print("{:02x}".format(int(plain_str[idx:idx+8],2)) , int(plain_str[idx:idx+8],2))
 
-                
-        return plain_hex
+        return plain
 
 
     def __encrypt_cmp__(a:list, b:list) -> bool:
@@ -149,6 +141,12 @@ class Transposition_Cipher3D:
         if a[1] != b[1]:
             rel = 1 if a[1] > b[1] else -1
             return rel
+        
+def to_ascii(ori:str):
+
+    bytes_object = bytes.fromhex(ori)
+    ascii_string = bytes_object.decode("ASCII")
+    print(ascii_string)
 
 def ciphertext_to_qrcode(cipher:str):
     img = qrcode.make(cipher)
@@ -165,11 +163,12 @@ def ciphertext_to_qrcode(cipher:str):
 # key_num = (three, four, five, six, seven)
 
 
-plaintext = "a1095514"
-key       = "aaaaaaaa"
+plaintext = "a1095514a1095514"
+key       = "zzzzzzzz"
 print(f"plaintext = {plaintext}, key = {key}")
 
 ciphertext_input = Transposition_Cipher3D.encrypt(plaintext, key)
+# print(f"ciphertext = {ciphertext_input}")
 ciphertext_to_qrcode(ciphertext_input)
 plaintext_output = Transposition_Cipher3D.decrypt(ciphertext_input, key)
 print(f"plaintext = {plaintext_output}")
